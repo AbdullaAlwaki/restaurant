44acef1220b1fab20fb627207310cb4e745a1e0d
Kamal <dearkamalpreet@gmail.com>
2023-01-24 10:57:31 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import NavBar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
-      welcome in frontend
+     <NavBar/>
     </div>
   );
 }
