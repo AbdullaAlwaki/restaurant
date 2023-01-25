842adb6cd9990d360a15854e01800ed0ab310af8
Romina <romina.lubjuhn@gmail.com>
2023-01-25 09:46:45 +0100

    table_booking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Booking from './components/Booking';
 import NavBar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Booking/>
      
     </div>
   );
