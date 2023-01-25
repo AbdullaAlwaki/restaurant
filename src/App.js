2e9f2ff67507ef56345f5cc1a98d20960f03b955
Kamal <dearkamalpreet@gmail.com>
2023-01-25 14:44:07 +0100

    carousel done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home.jsx';
 import NavBar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Home/>
     </div>
   );
 }
