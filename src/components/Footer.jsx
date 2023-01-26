93d16a196d4b51807528bf300f3d510b603c4ff1
Ahmed Omar <ahmedomaryara10@gmail.com>
2023-01-26 14:41:14 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,7 @@ import "../styles/Footer.css";
 
 
 function Footer() {
-    const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.2482974880027!2d13.441952384194233!3d52.47463987980567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84fe357b7b88d%3A0xac3788f704e9f660!2z2KfZhNmF2LfYudmFINin2YTZitmF2YbZig!5e0!3m2!1sar!2sde!4v1674637960897!5m2!1sar!2sde"
-    " 
-      style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> `
+    const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.248117032569!2d13.437575016005983!3d52.474643147341666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84fe357b7b88d%3A0xac3788f704e9f660!2sJemenitisches%20Restaurant!5e0!3m2!1sen!2sde!4v1674737063564!5m2!1sen!2sde" ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
     /* const [isNavExpanded, setIsNavExpanded] = useState(false); */
     return<footer>
             <div className="Fup" >
