5a5e4a5f26e2869fd4a716c9f51d2b446206f631
Romina <romina.lubjuhn@gmail.com>
2023-02-01 12:23:28 +0100

    fixed offer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home/Home.jsx';
 import Menu from './components/Menu/Menu.jsx';
 import Footer from './components/Footer.jsx';
 import Services from './components/Services/Services.jsx';
+import Contact from './components/Contact/Contact';
 import AddDishes from './Admin/AddDishes.jsx';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route  path='/' element={<Home/>}/>
         <Route path='/menu' element={<Menu/>}/>
         <Route path='/services' element={<Services/>}/>
+        <Route path='/contact' element={<Contact/>}/>
         <Route path='/addDishes' element={<AddDishes/>}/>
        </Routes>
       </div>
