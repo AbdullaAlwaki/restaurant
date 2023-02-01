d8357f5c762766f358e6fbb4891fdb16d3e6a74d
Romina <romina.lubjuhn@gmail.com>
2023-02-01 23:05:05 +0100

    contact page finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import "../../styles/Contact.css"
+import openingTimes from "../../images/openingTimes.png"
+import address from "../../images/address.png"
+import phoneNumber from "../../images/phoneNumber.png"
+import email from "../../images/email.png"
 
 function Contact() {
     return (
-        <div>
+        <div className="containerContact">
             <div className="smallCon">
-                <section className="openingCon">
-                    <img src="" alt="" />
-                    <h4>Opening Times</h4>
-                    <p></p>
+                <section className="smallConIns">
+                    <img src={openingTimes} alt="opening_logo" className="logoContact"/>
+                    <h3 className="headingIcon">Opening Times</h3>
+                    <p className="textIcon">Monday - Friday: <br/> 09:00 - 22:00 <br/> Saturday & Sunday: <br/> 10:00 - 24:00</p>
                 </section>
-                <section className="addressCon">
-                    <img src="" alt="" />
-                    <h4>Address</h4>
-                    <p>Karl-Max-Straße 172, 12043 Berlin</p>
+                <section className="smallConIns">
+                    <img src={address} alt="adress_logo" className="logoContact"/>
+                    <h3 className="headingIcon">Address</h3>
+                    <p className="textIcon">Karl-Max-Straße 172 <br/> 12043 Berlin</p>
                 </section>
-                <section className="phoneCon">
-                    <img src="" alt="" />
-                    <h4>Phone Number</h4>
-                    <p>+49 30 12345678</p>
+                <section className="smallConIns">
+                    <img src={phoneNumber} alt="phone_logo" className="logoContact"/>
+                    <h3 className="headingIcon">Phone Number</h3>
+                    <p className="textIcon">+49 30 12345678</p>
                 </section>
-                <section className="mailCon">
-                    <img src="" alt="" />
-                    <h4>Email</h4>
-                    <p>mern@gmail.com</p>
+                <section className="smallConIns">
+                    <img src={email} alt="email_logo" className="logoContact"/>
+                    <h3 className="headingIcon">Email</h3>
+                    <p className="textIcon">mern@gmail.com</p>
                 </section>
             </div>
-            <div className="contactField"></div>
+            <div className="contactField">
+                <p className="headingContact">Contact Us</p>
+                <form action="" className="formContact">
+                    <input type="text" name="name" className="inputContact" placeholder="Your name"/>
+                    <input type="email" name="email" id="" className="inputContact" placeholder="Your email" />
+                    <input type="text" name="textfield" className="inputContact" placeholder="Your message"/>
+                    <button type="submit" className="buttonContact">Submit</button>
+                </form>
+            </div>
         </div>
     )
 }
