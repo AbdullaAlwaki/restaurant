fe6baf36dcca8c3b3800822bd09ade04ef35a34c
Abdulla Alwaki <Abdullaalwaki@icloud.com>
2023-02-07 12:00:52 +0100

    merge ahmed branch

--- src/components/Home/Fav.dishes.jsx
+++ src/components/Home/Fav.dishes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import {dishes} from "../../help/help.jsx";
-import "../../styles/Fav.dishes.css";
+import "../../styles/Home/Fav.dishes.css";
 
 
 
