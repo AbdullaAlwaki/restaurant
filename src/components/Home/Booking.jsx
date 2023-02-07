96925cdc8ee763d8a573ba584fbbe10d8c0ab24c
Romina <romina.lubjuhn@gmail.com>
2023-02-07 10:23:24 +0100

    changes booking

--- src/components/Home/Booking.jsx
+++ src/components/Home/Booking.jsx
@@ -2,18 +2,38 @@ import React from "react";
 import "../../styles/Home/Booking.css";
 
 function Booking() {
+    const booking = [
+        {
+            type: "text",
+            name: "name",
+            label: "Name",
+        },
+        {
+            type: "number",
+            name: "amountOfPerson",
+            label: "How many person?",
+        },
+        {
+            type: "time",
+            name: "time",
+        },
+        {
+            type: "date",
+            name: "date",
+        },
+    ];
     return (
         <div className="booking">
             <h2 className="headingBook">Book a table now</h2>
-            <div className="form">
-            {/*name*/}
-            <input type="text" placeholder="Name" className="inputBook"/>
-            {/*number of person*/}
-            <input type="number" placeholder="How many person?" className="inputBook"/>
-            {/*time for booking */}
-            <input type="time" placeholder="What time?" className="inputBook"/>
-            {/*day for booking*/}
-            <input type="date" className="inputBook" />
+            <div className="container_booking">
+                <form className="formBooking">
+                    {booking.map((item, index) => (
+                       <div key={index} className="floating_group">
+                            <input type={item.type} name={item.name} className="inputBook" required/>
+                            <label className="labelBook">{item.label}</label> 
+                        </div>
+                    ))}
+                </form>
             {/*reservation button*/}
             <button type="submit" className="buttonBook">Book now</button>
             </div>
