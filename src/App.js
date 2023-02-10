29a6b5a0d0f195ba24f5ea2b5487875b59a9ad3e
Kamal <dearkamalpreet@gmail.com>
2023-02-10 16:02:41 +0100

    done

--- src/App.js
+++ src/App.js
@@ -11,17 +11,8 @@ import AddDishes from './Admin/AddDishes.jsx';
 import Register from './components/Register/Register';
 import Order from './components/Order';
 import Context from './components/Context/context';
-import React from "react";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/Navbar.jsx";
-import Home from "./components/Home/Home.jsx";
+
 import About from "./components/About/About.jsx";
-import Menu from "./components/Menu/Menu.jsx";
-import Footer from "./components/Footer.jsx";
-import Services from "./components/Services/Services.jsx";
-import Contact from "./components/Contact/Contact";
-import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import Dashboard from "./Admin/Dashboard.jsx";
 
